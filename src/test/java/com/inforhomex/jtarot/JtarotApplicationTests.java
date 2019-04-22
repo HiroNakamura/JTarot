889461aed7c7d7f515c9b969216f281b6301ad75
ARIEL <carraro.ariel@gmail.com>
2019-04-22 13:01:16 -0500

    Se creo archivo de test

---
+++ src/test/java/com/inforhomex/jtarot/JtarotApplicationTests.java
@@ -0,0 +1,16 @@
+package com.inforhomex.jtarot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JtarotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
