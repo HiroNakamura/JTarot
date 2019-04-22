7646519973ec7ec9328fd3e4cf63b46c84171f0f
ARIEL <carraro.ariel@gmail.com>
2019-04-22 12:59:04 -0500

    Se creo archivo principal de la aplicación

---
+++ src/main/java/com/inforhomex/jtarot/JtarotApplication.java
@@ -0,0 +1,13 @@
+package com.inforhomex.jtarot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtarotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtarotApplication.class, args);
+	}
+
+}
