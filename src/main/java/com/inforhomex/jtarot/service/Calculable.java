0d498fa66301ef4fc498febdd02abdaf4e3b27f6
ARIEL <carraro.ariel@gmail.com>
2019-04-23 12:19:58 -0500

    Se agregó método getDescripcion

--- src/main/java/com/inforhomex/jtarot/service/Calculable.java
+++ src/main/java/com/inforhomex/jtarot/service/Calculable.java
@@ -1,7 +1,7 @@
 package com.inforhomex.jtarot.service;
 
-
 public interface Calculable{
 	String getCalculo(String nombre, String fecha);
 	String getTarot(String nombre, String fecha);
+	String getDescripcion(String nombre, String fecha);
 }
