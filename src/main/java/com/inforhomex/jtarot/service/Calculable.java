1de95b33f1a535bf94416529f31cc36aeb7d295b
ARIEL <carraro.ariel@gmail.com>
2019-05-28 14:06:14 -0500

    Se edito servicio

--- src/main/java/com/inforhomex/jtarot/service/Calculable.java
+++ src/main/java/com/inforhomex/jtarot/service/Calculable.java
@@ -2,6 +2,6 @@ package com.inforhomex.jtarot.service;
 
 public interface Calculable{
 	String getCalculo(String nombre, String fecha);
-	String getTarot(String nombre, String fecha);
+	String getTarot(String fecha);
 	String getDescripcion(String nombre, String fecha);
 }
