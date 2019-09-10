a04f985570a208ca507b2f8cbdf2d120ee8569d4
ARIEL <carraro.ariel@gmail.com>
2019-09-09 20:42:05 -0500

    ControllerPrincipal.java editado

--- src/main/java/com/inforhomex/jtarot/controller/ControllerPrincipal.java
+++ src/main/java/com/inforhomex/jtarot/controller/ControllerPrincipal.java
@@ -2,13 +2,13 @@ package com.inforhomex.jtarot.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.servlet.ModelAndView;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -17,12 +17,12 @@ import com.inforhomex.jtarot.service.Calculable;
 @Controller
 @RequestMapping("/jtarot")
 public class ControllerPrincipal{
-
-	@Autowired
+    
+    @Autowired
     private Calculable calculable;
 	
-	//http://localhost:8090/jtarot/index
-	@GetMapping("/index")
+    //http://localhost:8090/jtarot/index
+    @GetMapping({"/index","/","/index.html"})
     public String index(){
         return "index";
     }
