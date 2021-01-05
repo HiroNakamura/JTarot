919807f44fc5edb45e00c5def8a718c0962f791c
Fer_Carraro <carraro.fernando@gmail.com>
2021-01-04 20:32:17 -0600

    Se cambio el nombre del archivo YAML, se creao un RestController y la clase Carta

---
+++ src/main/java/com/inforhomex/jtarot/model/Carta.java
@@ -0,0 +1,5 @@
+package com.inforhomex.jtarot.model;
+
+public class Carta{
+
+}
---
+++ src/main/java/com/inforhomex/jtarot/restcontroller/RestTarotController.java
@@ -0,0 +1,8 @@
+package com.inforhomex.jtarot.restcontroller;
+
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+public class RestTarotController{
+    
+}
