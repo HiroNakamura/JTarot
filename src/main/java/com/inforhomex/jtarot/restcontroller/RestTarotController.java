baa55b0078a29e35b97b6de5889060bc9c2bf5f4
Fer_Carraro <carraro.fernando@gmail.com>
2021-01-07 22:30:27 -0600

    Se ha editado: build.gradle y application.yaml

--- src/main/java/com/inforhomex/jtarot/restcontroller/RestTarotController.java
+++ src/main/java/com/inforhomex/jtarot/restcontroller/RestTarotController.java
@@ -1,8 +1,16 @@
 package com.inforhomex.jtarot.restcontroller;
 
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@RequestMapping("/tarotapi")
 @RestController
 public class RestTarotController{
     
+    //http://localhost:8090/tarotapi/saludo
+    @GetMapping("saludo")
+    public String saludo(){
+        return new String("Bienvenidos a JTarot");
+    }
 }
