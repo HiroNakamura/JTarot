0a3bd55334c736056372c97677e4c1a9e6e7ddd7
Fer_Carraro <carraro.fernando@gmail.com>
2021-01-09 10:09:32 -0600

    Se ha editado RestTarotController.java

--- src/main/java/com/inforhomex/jtarot/restcontroller/RestTarotController.java
+++ src/main/java/com/inforhomex/jtarot/restcontroller/RestTarotController.java
@@ -1,8 +1,17 @@
 package com.inforhomex.jtarot.restcontroller;
 
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+
+@RequestMapping("jtarotapi")
 @RestController
 public class RestTarotController{
     
+    //http://localhost:8090/jtarotapi/saludo
+    @GetMapping("/saludo")
+    public String saludo(){
+    	return new String("Bienvenido a JTarot!!");
+    }
 }
