326dbc44c730b7ecbec1955ab116b08b319d18b5
Fer_Carraro <carraro.fernando@gmail.com>
2021-01-09 16:20:57 -0600

    RestTarotController.java se ha editado

--- src/main/java/com/inforhomex/jtarot/restcontroller/RestTarotController.java
+++ src/main/java/com/inforhomex/jtarot/restcontroller/RestTarotController.java
@@ -1,22 +1,8 @@
 package com.inforhomex.jtarot.restcontroller;
 
 import org.springframework.web.bind.annotation.RestController;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-
-@RequestMapping("/tarotapi")
-@RestController
-public class RestTarotController{
-    
-    //http://localhost:8090/tarotapi/saludo
-    @GetMapping("saludo")
-    public String saludo(){
-        return new String("Bienvenidos a JTarot");
-=======
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 
 @RequestMapping("jtarotapi")
 @RestController
@@ -37,6 +23,5 @@ public class RestTarotController{
     	carta.setNombre("EL EMPERADOR");
     	carta.setDescripcion("La carta del empeador de las barajas del Tarot.");
     	return carta;
->>>>>>> bacd4382762dd4f8701b0817ea4fb30961b36229
     }
 }
